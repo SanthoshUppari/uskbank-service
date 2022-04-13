863deb2aab504f206fc7c8291a0b300ac6080ef4
Santhosh U <santhosh.u@ddistechnologies.com>
2022-04-13 10:58:17 +0530

    Initial project setup

---
+++ src/main/java/com/usk/uskbankservice/UskbankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.usk.uskbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UskbankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UskbankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usk/uskbankservice/UskbankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usk.uskbankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UskbankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
